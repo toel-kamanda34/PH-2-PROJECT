b938f4000168b1a1192f2f4d91760881525f6db9
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-16 02:29:15 +0300

    edited db.json and index.css

--- dream-catcher-app/src/components/Footer.js
+++ dream-catcher-app/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div>
        <footer className="footer">
-      <p>&copy; 2024 Developer Kamanda. All rights reserved.</p>
+      <p>&copy; 2024 Dream Catcher - Kamanda. All rights reserved.</p>
     </footer>
     </div>
   )
