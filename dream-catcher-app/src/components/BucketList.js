a957524cfd19979206c2d9902891119617bc06fa
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-16 00:41:56 +0300

    imported bucket list data

--- dream-catcher-app/src/components/BucketList.js
+++ dream-catcher-app/src/components/BucketList.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import logo from '../logo.png';
+
 import pin from '../pin.png';
 
-function BucketList() {
+function BucketList(props) {
   return (
     <div>
       <main class="bucket__lists">
         <section class="bucket__list">
           <div class="bucket__list__img">
-            <img src={logo} alt="pin"></img>
+            <img src={props.imageUrl} alt="pin"></img>
           </div>
           <div class="bucket__list__details">
            <div class="bucket__list__location">
             <img src={pin } alt="logo"></img>
-            <p class="bucket__list__location__country">Location</p>
+            <p class="bucket__list__location__country">{props.location}</p>
            </div>
-           <h2 class="bucket__list__header">Location header</h2>
-           <p class="bucket__list__date">234412</p>
-           <div class="bucket__list__summary"><p>was nice</p></div>
+           <h2 class="bucket__list__header">{props.title}</h2>
+           <p class="bucket__list__date">{props.startDate} - {props.endDate}</p>
+           <div class="bucket__list__summary"><p>{props.description}</p></div>
           </div>
         </section>
       </main>
