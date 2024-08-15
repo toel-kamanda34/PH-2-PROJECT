e83717c59d01781ad26f132aba47446b427ce54c
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-16 01:22:33 +0300

    map over bucket list data

--- dream-catcher-app/src/App.js
+++ dream-catcher-app/src/App.js
@@ -1,4 +1,5 @@
 
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import BucketList from './components/BucketList';
 import Footer from './components/Footer';
@@ -6,11 +7,36 @@ import './App.css';
 
 
 function App() {
+   // State to hold the fetched data
+   const [bucketListData, setBucketListData] = useState([]);
+
+   useEffect(() => {
+         fetch('http://localhost:3000/data') 
+       .then(response => response.json())
+       .then(data => {
+         setBucketListData(data); // Save the data to state
+         console.log(data);
+       })
+       .catch(error => {
+         console.error('Error fetching data:', error);
+       });
+   }, []); // Empty to ensure this runs onca
+ 
   return (
     <div className="App">
       <Header />
       <div class="bucket__lists">
-        <BucketList />
+        {bucketListData.map((item, index) => (
+          <BucketList 
+            key={index} 
+            title={item.title} 
+            location={item.location}
+            startDate={item.startDate}
+            endDate={item.endDate}
+            description={item.description}
+            imageUrl={item.imageUrl}
+        />
+      ))}
       </div>
       <Footer />
     </div>
